1073bb6bf7a7a1d85cbbf5df3983fae472880173
CapitaineToinon <antoinesutter@gmail.com>
2021-12-22 14:42:44 +0100

    Fixed constantes

--- src/DeclarationConstant.java
+++ src/DeclarationConstant.java
@@ -1,11 +1,13 @@
 public class DeclarationConstant extends Instruction {
 
     private Type type;
-    private Expression expression = null;
+    private Expression expression;
+    private Idf identifier;
 
-    public DeclarationConstant(Type type, Expression expression, String fl, int line, int col) {
+    public DeclarationConstant(Type type, Idf identifier, Expression expression, String fl, int line, int col) {
         super(fl, line, col);
         this.type = type;
+        this.identifier = identifier;
         this.expression = expression;
     }
 
